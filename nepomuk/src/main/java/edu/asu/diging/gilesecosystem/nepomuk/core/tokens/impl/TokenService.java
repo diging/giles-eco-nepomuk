77a02e3c0136026710943fea6a38e8b322f72c95
Suraj Nilapwar <suraj.nilapwar@asu.edu>
2017-01-26 10:18:49 -0700

    [GECO-1] Remove unnecessary functions and files

--- nepomuk/src/main/java/edu/asu/diging/gilesecosystem/nepomuk/core/tokens/impl/TokenService.java
+++ nepomuk/src/main/java/edu/asu/diging/gilesecosystem/nepomuk/core/tokens/impl/TokenService.java
@@ -1,12 +1,7 @@
 package edu.asu.diging.gilesecosystem.nepomuk.core.tokens.impl;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
-
-import java.util.Date;
 import java.util.UUID;
 
 import org.slf4j.Logger;
@@ -14,11 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import edu.asu.diging.gilesecosystem.nepomuk.core.users.User;
 import edu.asu.diging.gilesecosystem.nepomuk.core.apps.IRegisteredApp;
 import edu.asu.diging.gilesecosystem.nepomuk.core.service.properties.Properties;
 import edu.asu.diging.gilesecosystem.util.properties.IPropertiesManager;
-import edu.asu.diging.gilesecosystem.nepomuk.core.tokens.IApiTokenContents;
 import edu.asu.diging.gilesecosystem.nepomuk.core.tokens.IAppToken;
 import edu.asu.diging.gilesecosystem.nepomuk.core.tokens.ITokenService;
 
@@ -33,47 +26,8 @@ public class TokenService implements ITokenService {
     
     private final Logger logger = LoggerFactory.getLogger(getClass());
     
-    /**
-     * 4 hours
-     */
-    private int timeTillExpiration = 14400000;
-    
     @Autowired
     private IPropertiesManager propertiesManager;
-
-    @Override
-    public String generateApiToken(User user) {
-        String compactJws = Jwts.builder()
-                .setSubject(user.getUsername())
-                .setExpiration(new Date((new Date()).getTime() + timeTillExpiration))
-                .signWith(SignatureAlgorithm.HS512, propertiesManager.getProperty(Properties.SIGNING_KEY))
-                .compact();
-        
-        return compactJws;
-    }
-    
-    @Override
-    public IApiTokenContents getApiTokenContents(String token) {
-        IApiTokenContents contents = new ApiTokenContents();
-        contents.setExpired(true);
-        try {
-            Jws<Claims> jws = Jwts.parser().setSigningKey(propertiesManager.getProperty(Properties.SIGNING_KEY)).parseClaimsJws(token);
-            Claims claims = jws.getBody(); 
-            contents.setUsername(claims.getSubject());
-            Date expirationTime = claims.getExpiration();
-            if (expirationTime != null) {
-                contents.setExpired(expirationTime.before(new Date()));
-            }
-        } catch (io.jsonwebtoken.ExpiredJwtException e) {
-            logger.info("Token is expired.", e);
-            contents.setExpired(true); 
-        } catch (SignatureException e) {
-            logger.warn("Token signature not correct.", e);
-            return null;
-        } 
-        
-        return contents;
-    }
 
     @Override
     public IAppToken generateAppToken(IRegisteredApp app) {
@@ -91,25 +45,4 @@ public class TokenService implements ITokenService {
         token.setAppId(app.getId());
         return token;
     }
-    
-    @Override
-    public IAppToken getAppTokenContents(String token) {
-        IAppToken appToken = new AppToken();
-        
-        try {
-            Jws<Claims> jws = Jwts.parser().setSigningKey(propertiesManager.getProperty(Properties.SIGNING_KEY_APPS)).parseClaimsJws(token);
-            Claims claims = jws.getBody(); 
-            
-            appToken.setAppId(claims.get("appId", String.class));
-            appToken.setId(claims.get("tokenId", String.class));
-        
-        } catch (io.jsonwebtoken.ExpiredJwtException e) {
-            return null;
-        } catch (SignatureException e) {
-            return null;
-        } 
-        
-        return appToken;
-    }
-    
 }
