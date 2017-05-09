644d779b64dcd1daf1dc4f9d0d92ce05f88de48f
Julia Damerow <julia.damerow@googlemail.com>
2017-05-09 15:06:17 -0700

    [GECO-75] handle empty files correctly
    and always return a request even if there was an issue

---
+++ nepomuk/src/main/java/edu/asu/diging/gilesecosystem/nepomuk/core/exception/FileDownloadException.java
@@ -0,0 +1,36 @@
+package edu.asu.diging.gilesecosystem.nepomuk.core.exception;
+
+public class FileDownloadException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -3762594244426911109L;
+
+    public FileDownloadException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public FileDownloadException(String message, Throwable cause,
+            boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
+
+    public FileDownloadException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public FileDownloadException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public FileDownloadException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
